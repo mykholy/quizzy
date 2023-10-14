bf62982c00ab088ec045bb1047fe1b65ef52525e
mykholy <mykholys30@gmail.com>
2023-10-14 13:21:24 +0200

    =fix name footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=0'>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="Description" content="Bootstrap Responsive Admin Web Dashboard HTML5 Template">
-    <meta name="Author" content="Spruko Technologies Private Limited">
-    <meta name="Keywords" content="admin,admin dashboard,admin dashboard template,admin panel template,admin template,admin theme,bootstrap 4 admin template,bootstrap 4 dashboard,bootstrap admin,bootstrap admin dashboard,bootstrap admin panel,bootstrap admin template,bootstrap admin theme,bootstrap dashboard,bootstrap form template,bootstrap panel,bootstrap ui kit,dashboard bootstrap 4,dashboard design,dashboard html,dashboard template,dashboard ui kit,envato templates,flat ui,html,html and css templates,html dashboard template,html5,jquery html,premium,premium quality,sidebar bootstrap 4,template admin bootstrap 4">
+    <meta name="Description" content="{{setting('application_name')}}">
+    <meta name="Author" content="{{setting('application_name')}}">
+    <meta name="Keywords" content="{{setting('application_name')}}">
 
     <!-- Title -->
     <title>{{setting('application_name')}}</title>
@@ -1015,7 +1015,7 @@
     <!-- Footer opened -->
     <div class="main-footer">
         <div class="container-fluid pd-t-0 ht-100p">
-            <span> Copyright © {{date('Y')}} <a href="javascript:void(0);" class="text-primary">{{setting('application_name')}}</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="https://web.facebook.com/A2rSoft/"> A2rSoft </a> All rights reserved.</span>
+            <span> Copyright © {{date('Y')}} <a href="javascript:void(0);" class="text-primary">{{setting('application_name')}}</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="{{env('DEV_COMPANY_URL')}}"> {{env('DEV_COMPANY')}} </a> All rights reserved.</span>
         </div>
     </div>
     <!-- Footer closed -->
