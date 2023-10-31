5376d9fd21c826e755cb1ebdb193f769a1c714bf
mykholy <mykholys30@gmail.com>
2023-10-31 15:31:43 +0200

    =last update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Admin\AcademicYear;
+use App\Models\Admin\Group;
+use App\Models\Admin\Lesson;
+use App\Models\Admin\Question;
+use App\Models\Admin\Student;
+use App\Models\Admin\Subject;
+use App\Models\Admin\Teacher;
+use App\Models\Admin\Unit;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +31,16 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $cards = [
+            ['title' => __('models/teachers.plural'), 'color' => 'primary', 'count' => Teacher::count()],
+            ['title' => __('models/subjects.plural'), 'color' => 'warning', 'count' => Subject::count()],
+            ['title' => __('models/groups.plural'), 'color' => 'info', 'count' => Group::count()],
+            ['title' => __('models/students.plural'), 'color' => 'success', 'count' => Student::count()],
+            ['title' => __('models/academicYears.plural'), 'color' => 'danger', 'count' => AcademicYear::count()],
+            ['title' => __('models/units.plural'), 'color' => 'success', 'count' => Unit::count()],
+            ['title' => __('models/lessons.plural'), 'color' => 'warning', 'count' => Lesson::count()],
+            ['title' => __('models/questions.plural'), 'color' => 'primary', 'count' => Question::count()],
+        ];
+        return view('home', compact('cards'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,31 @@
 
     <div class="container-fluid">
         <h1 class="text-black-50">You are logged in!</h1>
-        <div class="row">
-
+        <div class="row row-sm">
+            @foreach($cards as $card)
+                <div class="col-xl-3 col-lg-6 col-md-6 col-xm-12">
+                    <div class="card overflow-hidden sales-card bg-{{$card['color']}}-gradient">
+                        <div class="px-3 pt-3  pb-2 pt-0">
+                            <div class="">
+                                <h6 class="mb-3 tx-12 text-white">{{$card['title']}}</h6>
+                            </div>
+                            <div class="pb-0 mt-0">
+                                <div class="d-flex">
+                                    <div class="">
+                                        <h4 class="tx-20 fw-bold mb-1 text-white">{{$card['count']}}</h4>
+{{--                                        <p class="mb-0 tx-12 text-white op-7">Compared to last week</p>--}}
+                                    </div>
+{{--                                    <span class="float-end my-auto ms-auto">--}}
+{{--												<i class="fas fa-arrow-circle-up text-white"></i>--}}
+{{--												<span class="text-white op-7"> +427</span>--}}
+{{--											</span>--}}
+                                </div>
+                            </div>
+                        </div>
+{{--                        <span id="compositeline" class="pt-1"><canvas width="254" height="30" style="display: inline-block; width: 254px; height: 30px; vertical-align: top;"></canvas></span>--}}
+                    </div>
+                </div>
+            @endforeach
 
 
         </div>
