18af1f130d13e6a65662519d726c60827f52b60e
mykholy <mykholys30@gmail.com>
2024-01-13 13:25:41 +0200

    =fix answer format

--- resources/views/admin/answers/answer_type_form/true_false.blade.php
+++ resources/views/admin/answers/answer_type_form/true_false.blade.php
@@ -8,6 +8,21 @@
     @endif
 </div>
 
+<div class="form-group col-sm-6 col-lg-6 mb-3">
+
+    {!! Form::label('title', __('models/answers.answer_view_format')) !!}
+
+    {!! Form::select('answer_view_format',App\Models\Admin\Answer::getAllAnswerViewFormat(),null, array('class' => 'form-control select2 select2-hidden-accessible','required'=>'required', 'ui-jp'=>"select2",'ui-options'=>"{theme: 'bootstrap'}" )) !!}
+    @if ($errors->has('answer_view_format'))
+        <span class="invalid-feedback">
+              <small class="text-danger">{{ $errors->first('answer_view_format') }}</small>
+
+         </span>
+    @endif
+
+
+</div>
+
 
 <!-- 'bootstrap / Toggle Switch is_correct Field' -->
 <div class="form-group col-sm-6">
