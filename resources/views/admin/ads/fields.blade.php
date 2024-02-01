12ec8ba9db042c3185777fc04853ac45103f2215
mykholy <mykholys30@gmail.com>
2024-02-01 19:40:19 +0200

    =add ads v2

--- resources/views/admin/ads/fields.blade.php
+++ resources/views/admin/ads/fields.blade.php
@@ -6,21 +6,21 @@
 
 <!-- Photo Field -->
 <div class="form-group col-sm-6">
-    {!! Form::label('photo', __('models/ads.fields.photo').':') !!}
-    <div class="input-group">
-        <div class="custom-file">
-            {!! Form::file('photo', ['class' => 'custom-file-input']) !!}
-            {!! Form::label('photo', 'Choose file', ['class' => 'custom-file-label']) !!}
-        </div>
+    {!! Form::label('photo', __('models/academicYears.fields.photo').':') !!}
+    <div class="mb-3">
+        {!! Form::file('photo', ['class' => 'form-control']) !!}
     </div>
 </div>
 <div class="clearfix"></div>
-
 <!-- 'bootstrap / Toggle Switch Is Active Field' -->
 <div class="form-group col-sm-6">
-    <div class="custom-control custom-switch">
+
+    <div class="form-checkbox custom-control">
         {!! Form::hidden('is_active', 0) !!}
         {!! Form::checkbox('is_active', 1, null,  ['id'=>'is_active','class' => 'custom-control-input']) !!}
-        {!! Form::label('is_active', __('models/ads.fields.is_active').':', ['class' => 'custom-control-label']) !!}
+        {!! Form::label('is_active', __('models/academicYears.fields.is_active').':', ['class' => 'custom-control-label mt-1']) !!}
+
     </div>
+
 </div>
+
