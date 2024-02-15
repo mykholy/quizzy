d430d61a6d1237656eb6a07a6b9809e363998009
mykholy <mykholys30@gmail.com>
2024-02-16 01:20:22 +0200

    =fix parmenters v2

--- resources/views/admin/answers/answer_type_form/matching.blade.php
+++ resources/views/admin/answers/answer_type_form/matching.blade.php
@@ -35,22 +35,5 @@
 </div>
 
 
-<div class="form-group row">
-    <!-- answer_img Field -->
-    <div class="form-group row">
-        {!! Form::label('answer_img', __('backend.answer_img'),["class"=>"col-sm-2 form-control-label"]) !!}
-        <div class="col-sm-10">
-            {!! Form::file('answer_img',['accept'=>'.jpg, .png, image/jpeg, image/png','data-default-file'=>(isset($answer->answer_img)? URL::to('uploads/answers/'.$answer->answer_img):''),'data-plugins'=>"dropify", 'data-height'=>'150','class' => 'form-control dropify  ']) !!}
-        </div>
-    </div>
-
-    @if ($errors->has('answer_img'))
-        <span class="invalid-feedback">
-              <small class="text-danger">{{ $errors->first('answer_img') }}</small>
-         </span>
-    @endif
-
-</div>
-
 
 
