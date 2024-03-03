c713f1744eb9341cd6dab30f44219aa6c7d15918
mykholy <mykholys30@gmail.com>
2024-03-04 00:08:32 +0200

    =add bulk import questions v4

--- resources/views/admin/questions/bulkImport.blade.php
+++ resources/views/admin/questions/bulkImport.blade.php
@@ -25,13 +25,20 @@
             <div class="card-body">
 
                 <div class="row">
-                    <div class="alert alert-primary d-flex align-items-center" role="alert"> <svg class="flex-shrink-0 me-2 svg-primary" xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 0 24 24" width="1.5rem" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
-                        <div> type : {{implode(' | ',collect(\App\Models\Admin\Question::getAllTypes())->keys()->toArray())}} </div>
+                    <div class="alert alert-primary" style="color: #004085;background-color: #cce5ff;border-color: #b8daff;margin-bottom:0;margin-top:10px;">
+                        <svg class="flex-shrink-0 me-2 svg-primary" xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 0 24 24" width="1.5rem" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
+                        <strong>Step 1:</strong>
+                        <p>1. Download the skeleton file and fill it with proper data.</p>
+                        <p>2. You can download the example file to understand how the data must be filled.</p>
+                        <p>3. Once you have downloaded and filled the skeleton file, upload it in the form below and submit.</p>
+                        <p> type : {{implode(' | ',collect(\App\Models\Admin\Question::getAllTypes())->keys()->toArray())}} </p>
+                        <p> level : {{implode(' | ',collect(\App\Models\Admin\Question::getAllLevel())->keys()->toArray())}} </p>
+                    </div>
+                    <br>
+                    <div class="">
+                        <a href="{{ asset('download/question_bulk_demo.xlsx') }}" download><button class="btn btn-info">Download CSV</button></a>
+                    </div>
 
-                    </div>
-                    <div class="alert alert-primary d-flex align-items-center" role="alert"> <svg class="flex-shrink-0 me-2 svg-primary" xmlns="http://www.w3.org/2000/svg" height="1.5rem" viewBox="0 0 24 24" width="1.5rem" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"></path></svg>
-                        <div> level : {{implode(' | ',collect(\App\Models\Admin\Question::getAllLevel())->keys()->toArray())}} </div>
-                    </div>
                     <!-- file Field -->
                     <div class="form-group col-sm-6">
                         {!! Form::label('bulk_file', __('models/questions.fields.file').':') !!}
