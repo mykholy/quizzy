2c23d8e70b7b79c1b6c7be64b2e83ed7a1925621
mykholy <mykholys30@gmail.com>
2024-03-07 00:01:26 +0200

    =add chat to group v4

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -55,7 +55,7 @@ class GroupController extends AppBaseController
 
         /* add multiple participants */
         if ($group->students) {
-            Chat::conversation($conversation)->addParticipants( $group->students->toArray());
+            Chat::conversation($conversation)->addParticipants( $group->students->get());
         }
 
         session()->flash('success', __('messages.saved', ['model' => __('models/groups.singular')]));
@@ -129,7 +129,7 @@ class GroupController extends AppBaseController
         /* add multiple participants */
         if ($group->students) {
             $conversation = Chat::conversations()->getById($group->conversation_id);
-            Chat::conversation($conversation)->addParticipants( $group->students->toArray());
+            Chat::conversation($conversation)->addParticipants( $group->students->get());
         }
 
         session()->flash('success', __('messages.updated', ['model' => __('models/groups.singular')]));
