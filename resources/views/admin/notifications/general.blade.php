d2ef1b2aaf37d07fb7a80c8ef65a4e32740c6530
mykholy <mykholys30@gmail.com>
2024-03-10 13:56:23 +0200

    =add notifications

--- resources/views/admin/notifications/general.blade.php
+++ resources/views/admin/notifications/general.blade.php
@@ -1,6 +1,6 @@
 <!-- title Field -->
 <div class="form-group col-sm-12 col-lg-12 mb-3">
-    {!! Form::label('title', __('models/fields.notifications.title').':') !!}
+    {!! Form::label('title', __('models/notifications.fields.title').':') !!}
     {!! Form::text('title', null, ['class' => 'form-control  '. ($errors->has('title')?' is-invalid ':'')]) !!}
 
     @if ($errors->has('title'))
@@ -12,7 +12,7 @@
     @endif
 </div>
 <div class="form-group col-sm-12 col-lg-12 mb-3">
-    {!! Form::label('body', __('models/fields.notifications.body').':') !!}
+    {!! Form::label('body', __('models/notifications.fields.body').':') !!}
     {!! Form::text('body', null, ['class' => 'form-control  '. ($errors->has('body')?' is-invalid ':'')]) !!}
 
     @if ($errors->has('body'))
