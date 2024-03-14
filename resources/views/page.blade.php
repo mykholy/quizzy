1d072becd1d960b377067fae41cd205448ec7911
mykholy <mykholys30@gmail.com>
2024-03-14 17:56:17 +0200

    =fix

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -17,7 +17,22 @@
         <link href="https://www.freeprivacypolicy.com/public/livelink/css/livelink.css" rel="stylesheet" />
 
     </head>
-    <body class="antialiased">
-{!! setting($page) !!}
+    <body >
+    <div class="page">
+        <div class="header">
+            <div class="container">
+                <p class="title">{{ucwords($page)}} for Quizzy</p>
+            </div>
+        </div>
+        <div class="translations-content-container">
+            <div class="container">
+                <div class="tab-content translations-content-item en visible" id="en">
+                    {!! setting($page) !!}
+                </div>
+            </div>
+        </div>
+
+    </div>
+
     </body>
 </html>
